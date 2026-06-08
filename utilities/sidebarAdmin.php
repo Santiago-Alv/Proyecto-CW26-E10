26b178c72c468b1e9fb4be343f54beb4f27bcfa4
Abraham Nava Sandoval <325300944@alumno.enp.unam.mx>
2026-06-08 02:41:14 -0600

    feat(searchProfe): añade pagina y hyperlinks en sidebar para la pagina de search profesores

--- utilities/sidebarAdmin.php
+++ utilities/sidebarAdmin.php
@@ -5,8 +5,8 @@
     <aside class="sidebar">
         <nav>
             <ul>
-                <li><a href="../Templates/admin/searchAlAdmin.php" class="active">Alumnos</a></li>
-                <li><a href="#">Profesores</a></li>
+                <li><a href="searchAlAdmin.php">Alumnos</a></li>
+                <li><a href="searchPrf.php">Profesores</a></li>
                 <li><a href="#">Recursos</a></li>
                 <li><a href="#">Foro de dudas</a></li>
                 <li><a href="#">Crear grupo</a></li>
