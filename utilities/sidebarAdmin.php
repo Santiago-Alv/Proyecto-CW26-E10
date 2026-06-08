e296dbba65b7874fa661a595d5a48a823e4c0b71
Abraham Nava Sandoval <325300944@alumno.enp.unam.mx>
2026-06-08 02:33:32 -0600

    feat(adminpage): agrega navbar, sidebar, estilos y primera pagina de busqueda de alumnos por parte de user admin

---
+++ utilities/sidebarAdmin.php
@@ -0,0 +1,15 @@
+<?php
+// sidebarAdmin.php
+?>
+
+    <aside class="sidebar">
+        <nav>
+            <ul>
+                <li><a href="../Templates/admin/searchAlAdmin.php" class="active">Alumnos</a></li>
+                <li><a href="#">Profesores</a></li>
+                <li><a href="#">Recursos</a></li>
+                <li><a href="#">Foro de dudas</a></li>
+                <li><a href="#">Crear grupo</a></li>
+            </ul>
+        </nav>
+    </aside>
