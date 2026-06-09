8ee3d770e8f511d1404e98a70ea6f23403e61be8
Otomi26 <324490512@alumno.enp.unam.mx>
2026-06-09 15:33:01 -0600

    docs(sql): Añadir tablas faltantes de drawio a la base de datos nodeserte y respaldar a nodeserte2

--- docs/crear_tablas.sql
+++ docs/crear_tablas.sql
@@ -33,3 +33,71 @@ CREATE TABLE grupo(
 INSERT INTO grupo (nombre_grupo, id_profesor, modulo_activo) VALUES 
 ("61B", 1, 3), ("61D", 2, 3);
 
+-- Definición de la tabla alumno
+CREATE TABLE alumno(
+    id_alumno INT PRIMARY KEY AUTO_INCREMENT,
+    nocta INT NOT NULL, 
+    nombre VARCHAR(60) NOT NULL,
+    asistencia INT,
+    id_grupo INT NOT NULL,
+    FOREIGN KEY (id_grupo) REFERENCES grupo(id_grupo)
+);
+INSERT INTO alumno(nocta, nombre, asistencia, id_grupo) VALUES 
+(324487285, "Juanito Juanito", 10, 1);
+
+-- Definición de la tabla calif_mod (calificacion de modulo)
+CREATE TABLE calif_mod(
+    id_calif INT PRIMARY KEY AUTO_INCREMENT,
+    id_alumno INT NOT NULL,
+    id_modulo INT NOT NULL,
+    calificacion INT,
+    FOREIGN KEY (id_alumno) REFERENCES alumno(id_alumno),
+    FOREIGN KEY (id_modulo) REFERENCES modulo(id_modulo)
+);
+INSERT INTO calif_mod(id_alumno, id_modulo, calificacion) VALUES 
+(1, 1, 10);
+
+-- Definición de la tabla asistencia
+CREATE TABLE asistencia(
+    id_asistencia INT PRIMARY KEY AUTO_INCREMENT,
+    id_alumno INT NOT NULL,
+    id_modulo INT NOT NULL,
+    estatus CHAR(1),
+    fecha DATE NOT NULL,
+    FOREIGN KEY (id_alumno) REFERENCES alumno(id_alumno),
+    FOREIGN KEY (id_modulo) REFERENCES modulo(id_modulo)
+);
+INSERT INTO asistencia(id_alumno, id_modulo, estatus) VALUES 
+(1, 1, "J");
+
+-- Definición de la tabla estado de animo
+CREATE TABLE estadodeanimo(
+    id_animo INT PRIMARY KEY AUTO_INCREMENT,
+    id_alumno INT NOT NULL,
+    fecha DATE NOT NULL,
+    emocion VARCHAR(10) NOT NULL,
+    FOREIGN KEY (id_alumno) REFERENCES alumno(id_alumno)
+);
+INSERT INTO estadodeanimo(id_alumno, emocion) VALUES 
+(1,"Feliz");
+
+-- Definición de la tabla duda
+CREATE TABLE duda(
+    id_duda INT PRIMARY KEY AUTO_INCREMENT,
+    estado_duda CHAR(2) NOT NULL,
+    duda_text VARCHAR(2000) NOT NULL,
+    respuesta VARCHAR(2000)  NOT NULL,
+    id_alumno INT NOT NULL,
+    id_grupo INT NOT NULL,
+    id_profesor INT NOT NULL,
+    FOREIGN KEY (id_alumno) REFERENCES alumno(id_alumno),
+    FOREIGN KEY (id_grupo) REFERENCES grupo(id_grupo),
+    FOREIGN KEY (id_profesor) REFERENCES profesor(id_profesor)
+);
+INSERT INTO duda(id_alumno,id_grupo,id_profesor,estado_duda, duda_text, respuesta) VALUES 
+(1, 1, 1, "M","Que es git", "Es un controlador de versiones");
+
+
+
+
+
---
+++ docs/nodeserte2.sql
@@ -0,0 +1,259 @@
+-- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: nodeserte
+-- ------------------------------------------------------
+-- Server version	10.4.32-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `alumno`
+--
+
+DROP TABLE IF EXISTS `alumno`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `alumno` (
+  `id_alumno` int(11) NOT NULL AUTO_INCREMENT,
+  `nocta` int(11) NOT NULL,
+  `nombre` varchar(60) NOT NULL,
+  `asistencia` int(11) DEFAULT NULL,
+  `id_grupo` int(11) NOT NULL,
+  PRIMARY KEY (`id_alumno`),
+  KEY `id_grupo` (`id_grupo`),
+  CONSTRAINT `alumno_ibfk_1` FOREIGN KEY (`id_grupo`) REFERENCES `grupo` (`id_grupo`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alumno`
+--
+
+LOCK TABLES `alumno` WRITE;
+/*!40000 ALTER TABLE `alumno` DISABLE KEYS */;
+INSERT INTO `alumno` VALUES (1,324487285,'Juanito Juanito',10,1);
+/*!40000 ALTER TABLE `alumno` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `asistencia`
+--
+
+DROP TABLE IF EXISTS `asistencia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `asistencia` (
+  `id_asistencia` int(11) NOT NULL AUTO_INCREMENT,
+  `id_alumno` int(11) NOT NULL,
+  `id_modulo` int(11) NOT NULL,
+  `estatus` char(1) DEFAULT NULL,
+  `fecha` date NOT NULL,
+  PRIMARY KEY (`id_asistencia`),
+  KEY `id_alumno` (`id_alumno`),
+  KEY `id_modulo` (`id_modulo`),
+  CONSTRAINT `asistencia_ibfk_1` FOREIGN KEY (`id_alumno`) REFERENCES `alumno` (`id_alumno`),
+  CONSTRAINT `asistencia_ibfk_2` FOREIGN KEY (`id_modulo`) REFERENCES `modulo` (`id_modulo`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `asistencia`
+--
+
+LOCK TABLES `asistencia` WRITE;
+/*!40000 ALTER TABLE `asistencia` DISABLE KEYS */;
+INSERT INTO `asistencia` VALUES (1,1,1,'J','0000-00-00');
+/*!40000 ALTER TABLE `asistencia` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `calif_mod`
+--
+
+DROP TABLE IF EXISTS `calif_mod`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `calif_mod` (
+  `id_calif` int(11) NOT NULL AUTO_INCREMENT,
+  `id_alumno` int(11) NOT NULL,
+  `id_modulo` int(11) NOT NULL,
+  `calificacion` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_calif`),
+  KEY `id_alumno` (`id_alumno`),
+  KEY `id_modulo` (`id_modulo`),
+  CONSTRAINT `calif_mod_ibfk_1` FOREIGN KEY (`id_alumno`) REFERENCES `alumno` (`id_alumno`),
+  CONSTRAINT `calif_mod_ibfk_2` FOREIGN KEY (`id_modulo`) REFERENCES `modulo` (`id_modulo`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `calif_mod`
+--
+
+LOCK TABLES `calif_mod` WRITE;
+/*!40000 ALTER TABLE `calif_mod` DISABLE KEYS */;
+INSERT INTO `calif_mod` VALUES (1,1,1,10);
+/*!40000 ALTER TABLE `calif_mod` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `duda`
+--
+
+DROP TABLE IF EXISTS `duda`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `duda` (
+  `id_duda` int(11) NOT NULL AUTO_INCREMENT,
+  `estado_duda` char(2) NOT NULL,
+  `duda_text` varchar(2000) NOT NULL,
+  `respuesta` varchar(2000) NOT NULL,
+  `id_alumno` int(11) NOT NULL,
+  `id_grupo` int(11) NOT NULL,
+  `id_profesor` int(11) NOT NULL,
+  PRIMARY KEY (`id_duda`),
+  KEY `id_alumno` (`id_alumno`),
+  KEY `id_grupo` (`id_grupo`),
+  KEY `id_profesor` (`id_profesor`),
+  CONSTRAINT `duda_ibfk_1` FOREIGN KEY (`id_alumno`) REFERENCES `alumno` (`id_alumno`),
+  CONSTRAINT `duda_ibfk_2` FOREIGN KEY (`id_grupo`) REFERENCES `grupo` (`id_grupo`),
+  CONSTRAINT `duda_ibfk_3` FOREIGN KEY (`id_profesor`) REFERENCES `profesor` (`id_profesor`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `duda`
+--
+
+LOCK TABLES `duda` WRITE;
+/*!40000 ALTER TABLE `duda` DISABLE KEYS */;
+INSERT INTO `duda` VALUES (3,'M','Que es git','Es un controlador de versiones',1,1,1);
+/*!40000 ALTER TABLE `duda` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `estadodeanimo`
+--
+
+DROP TABLE IF EXISTS `estadodeanimo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `estadodeanimo` (
+  `id_animo` int(11) NOT NULL AUTO_INCREMENT,
+  `id_alumno` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `emocion` varchar(10) NOT NULL,
+  PRIMARY KEY (`id_animo`),
+  KEY `id_alumno` (`id_alumno`),
+  CONSTRAINT `estadodeanimo_ibfk_1` FOREIGN KEY (`id_alumno`) REFERENCES `alumno` (`id_alumno`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `estadodeanimo`
+--
+
+LOCK TABLES `estadodeanimo` WRITE;
+/*!40000 ALTER TABLE `estadodeanimo` DISABLE KEYS */;
+INSERT INTO `estadodeanimo` VALUES (1,1,'0000-00-00','Feliz');
+/*!40000 ALTER TABLE `estadodeanimo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `grupo`
+--
+
+DROP TABLE IF EXISTS `grupo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `grupo` (
+  `id_grupo` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_grupo` char(3) NOT NULL,
+  `id_profesor` int(11) NOT NULL,
+  `modulo_activo` int(11) NOT NULL,
+  PRIMARY KEY (`id_grupo`),
+  KEY `id_profesor` (`id_profesor`),
+  KEY `modulo_activo` (`modulo_activo`),
+  CONSTRAINT `grupo_ibfk_1` FOREIGN KEY (`id_profesor`) REFERENCES `profesor` (`id_profesor`),
+  CONSTRAINT `grupo_ibfk_2` FOREIGN KEY (`modulo_activo`) REFERENCES `modulo` (`id_modulo`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `grupo`
+--
+
+LOCK TABLES `grupo` WRITE;
+/*!40000 ALTER TABLE `grupo` DISABLE KEYS */;
+INSERT INTO `grupo` VALUES (1,'61B',1,3),(2,'61D',2,3);
+/*!40000 ALTER TABLE `grupo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `modulo`
+--
+
+DROP TABLE IF EXISTS `modulo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `modulo` (
+  `id_modulo` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_modulo` varchar(30) NOT NULL,
+  `modulo_activo` int(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id_modulo`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `modulo`
+--
+
+LOCK TABLES `modulo` WRITE;
+/*!40000 ALTER TABLE `modulo` DISABLE KEYS */;
+INSERT INTO `modulo` VALUES (1,'SISTEMAS OPERATIVOS',0),(2,'PROGRAMACION ESTRUCTURADA',0),(3,'INTRODUCCION A LA COMPUTACION',0);
+/*!40000 ALTER TABLE `modulo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `profesor`
+--
+
+DROP TABLE IF EXISTS `profesor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `profesor` (
+  `id_profesor` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_profesor` varchar(60) NOT NULL,
+  PRIMARY KEY (`id_profesor`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `profesor`
+--
+
+LOCK TABLES `profesor` WRITE;
+/*!40000 ALTER TABLE `profesor` DISABLE KEYS */;
+INSERT INTO `profesor` VALUES (1,'ANGIE'),(2,'CARLOS ALF');
+/*!40000 ALTER TABLE `profesor` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-06-09 15:28:15
