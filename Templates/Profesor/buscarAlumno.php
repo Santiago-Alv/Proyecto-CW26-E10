034207c22fd1ba2b8663ad7f709aea6eae518dc6
Santiago-Alv <214873111+Santiago-Alv@users.noreply.github.com>
2026-06-10 13:11:38 -0600

    feat(mult): agrega archivos y su estructura basica

---
+++ Templates/Profesor/buscarAlumno.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['usuario']) || $_SESSION['tipo'] !== 'profesor') {
+    
+    //header("Location: ../index.html"); 
+    // echo ¨vete wei no eres porfe¨
+    // Matar pa q no cargue lo demas
+    //exit(); 
+}
+?>
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href = "..\..\Statics\Css\profesorGraph.css">
+        <title>Home</title>
+    </head>
+    <body>
+        <header>
+            <img class="logo" src= "..\..\Statics\img\logo-unam.png">
+
+            <a href="..\index.php">Cerrar Sesión</a>
+        </header>
+    
+        <nav>
+            <div class="breadcrumbs">
+                <a href="./homeProfesor.php">🏠</a>
+                <span>&lt;</span>
+                <span>&gt;</span>
+                <span>Profesor Jirafales</span>
+            </div>
+        </nav>
+
+        <div class="mainCont">
+            <aside>
+                <a class="menuOp" href="./grupoProfesor.php"> 61B </a>
+                <a class="menuOp" href="./grupoProfesor.php"> 61D </a>
+                <a class="menuOp"> RECURSOS </a>
+                <a class="menuOp"> DUDAS </a>
+            </aside>
+
+            <main>
+            
+            </main>
+        </div>
+
+    </body>
+</html>
