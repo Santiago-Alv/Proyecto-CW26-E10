552966c87ec0f538bd5b6b9355c2d6deaf910562
Otomi26 <324490512@alumno.enp.unam.mx>
2026-06-11 19:03:47 -0600

    Añadir la barra lateral y parte superior de las paginas de alumno

--- Templates/alumno/ForoDudas.php
+++ Templates/alumno/ForoDudas.php
@@ -6,15 +6,18 @@
     //$modulo
 
     $duda = "";
-    if($_SERVER["REQUEST_METHOD"] == "POST" && isset($duda))
+    if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['pregunta']))
     {
         $duda = $_POST['pregunta'];
 
-        $sql = "INSERT INTO duda (id_duda,estado_duda, duda_text, respuesta, id_alumno, id_grupo, id_profesor)
-                VALUES (2, 'P','$duda', 'respuesta', 1,1,1)";
+        $sql = "INSERT INTO duda (estado_duda, duda_text, respuesta, id_alumno, id_grupo, id_profesor)
+                VALUES ('P','$duda', 'respuesta', 1,1,1)";
         $query = mysqli_query($conexion, $sql); 
 
     }
+
+    // placeholder
+    $nombre_alumno = "Juanito Juanito"; 
 
 ?>
 <!DOCTYPE html>
@@ -24,20 +27,25 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Foro de dudas de alumno</title>
         <link rel="stylesheet" href="../../Statics/Css/ForoDudas.css">
+        <link rel="stylesheet" href="../../Statics/Css/alumGraph.css">
     </head>
     <body>
-        <main id= "contenido">
-            <form action="" method = "POST" class="input-pregunta">
-                <label for="pregunta">¿Qué duda tienes? <br>Menciona el tema o la duda en general que tengas.<br></label>
-                <textarea name="pregunta" id="ipt-pregunta" placeholder="Escribe tu duda aquí" required></textarea>
-                <button type="submit" id="pregunta-submit">Enviar</button>
-            </form>     
-            <div class="botonesforo">
-                    <form action="historialpreguntas.php">
-                        <button type="submit" id="historial-submit">Ver historial</button>
-                    </form>
-            </div>           
-        </main>   
+        <?php include '../../utilities/navbarAlumn.php'; ?>
+        <div class="main-layout">
+            <?php include '../../utilities/sidebarAlumn.php'; ?>
+            <main id= "contenido">
+                <form action="" method = "POST" class="input-pregunta">
+                    <label for="pregunta">¿Qué duda tienes? <br>Menciona el tema o la duda en general que tengas.<br></label>
+                    <textarea name="pregunta" id="ipt-pregunta" placeholder="Escribe tu duda aquí" required></textarea>
+                    <button type="submit" id="pregunta-submit">Enviar</button>
+                </form>     
+                <div class="botonesforo">
+                        <form action="historialpreguntas.php">
+                            <button type="submit" id="historial-submit">Ver historial</button>
+                        </form>
+                </div>           
+            </main>  
+        </div>  
     </body>
 </html>
     
