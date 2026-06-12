f29269516866e1c27e7eb0b4ef3d54ef2bdafaa8
Sebastián Méndez Sánchez <sebmndz.04@gmail.com>
2026-06-11 20:19:40 -0600

    Agrega home, animo, forodudas, histdudas, mod 1 y 2, enlazado

---
+++ Templates/Alumno/ForoDudas.php
@@ -0,0 +1,39 @@
+<?php
+
+//session_start();
+
+
+// consulta db
+
+// placeholder
+$nombre_alumn = "Panchito"; 
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foro de dudas de alumno</title>
+    <link rel="stylesheet" href="../../Statics/Css/adminGraph.css">
+    <link rel="stylesheet" href="../../Statics/Css/ForoDudas.css">
+</head>
+<body>
+        <?php include '../../utilities/navbarAlumno.php'; ?>
+        <div class="main-layout">
+            <?php include '../../utilities/sidebarAlumno.php'; ?>
+
+        <main id= "contenido">
+            <div class="input-pregunta">
+                <label for="pregunta">¿Qué duda tienes? <br>Menciona el tema o la duda en general que tengas.<br></label>
+                <input type="text" name="pregunta" id="ipt-pregunta" placeholder="Escribe tu duda aquí" required>
+            </div> 
+            <div class="botonesforo">
+                <form action="historialpreguntas.php">
+                    <button type="submit" id="historial-submit">Ver historial</button>
+                </form>
+                <button type="submit" id="pregunta-submit">Enviar</button>
+            </div>
+        </main>   
+</body>
+</html>
+    
---
+++ Templates/Alumno/Modulo2Alumno.php
@@ -0,0 +1,56 @@
+<?php
+
+//session_start();
+
+
+// consulta db
+
+// placeholder
+$nombre_alumn = "Panchito"; 
+?>
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Modulo alumno</title>
+        <link rel="stylesheet" href="../../Statics/Css/adminGraph.css">
+        <link rel="stylesheet" href="../../Statics/Css/ModuloAlumno.css">
+    </head>
+    <body>
+        <?php include '../../utilities/navbarAlumno.php'; ?>
+        <div class="main-layout">
+            <?php include '../../utilities/sidebarAlumno.php'; ?>
+        <main id= "contenido">
+            <div id = "tituloModulo">
+                <h1>Modulo 2 - Tu estado</h1>
+            </div>
+            <div class = "tablas">
+                <div id = "asistencia">
+                    <table>
+                        <thead>
+                            <td>Asistencia de modulo</td>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>88%</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div id = "calificacion">
+                    <table>
+                        <thead>
+                            <td>Calificación modular</td>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>9</td>
+                            </tr>
+                    </table>
+                </div>
+            </div> 
+        </main>   
+    </body>
+</html>
+    
