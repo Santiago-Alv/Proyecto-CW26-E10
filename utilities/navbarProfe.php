769db94dbde20fd9abcb995e1790fc722dd4a4c3
iabraham120 <325300944@alumno.enp.unam.mx>
2026-06-12 19:48:14 -0600

    feat(recursos): añade pagina de recursos de profe y sidebar y navbar

---
+++ utilities/navbarProfe.php
@@ -0,0 +1,18 @@
+<header class="navbar-superior">
+    <div class="navbar-logo">
+        <img src="../../Statics/img/logo-unam.png" alt="Logo UNAM" class="img-logo">
+    </div>
+
+    <div class="navbar-logout">
+        <a href="../logout.php" class="btn-logout">Cerrar Sesión</a>
+    </div>
+</header>
+
+<div class="sub-navbar-ruta">
+    <div class="ruta-contenido">
+        <span class="icono-home">🏠</span> 
+        <span class="separador">&lt;</span> 
+        <span class="separador">&lt;</span> 
+        <span class="usuario-actual">Profesor Jirafales</span>
+    </div>
+</div>
---
+++ utilities/sidebarProfe.php
@@ -0,0 +1,10 @@
+
+<aside class="sidebar-izquierdo">
+    <div class="sidebar-menu">
+        <a href="grupo.php?g=61B" class="btn-sidebar <?php echo ($pagina_activa == '61B') ? 'activo' : ''; ?>">61B</a>
+        <a href="grupo.php?g=61D" class="btn-sidebar <?php echo ($pagina_activa == '61D') ? 'activo' : ''; ?>">61D</a>
+        
+        <a href="recursosProfesor.php" class="btn-sidebar <?php echo ($pagina_activa == 'recursos') ? 'activo' : ''; ?>">RECURSOS</a>
+        <a href="dudasProfesor.php" class="btn-sidebar <?php echo ($pagina_activa == 'dudas') ? 'activo' : ''; ?>">DUDAS</a>
+    </div>
+</aside>
