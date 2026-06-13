eb3a7b579ce2457bb14190da33b75b4e72ed8d0a
Otomi26 <324490512@alumno.enp.unam.mx>
2026-06-12 20:01:26 -0600

    docs(php): añadir sidebar de profesor

--- Templates/profe/ListaAsistencia.php
+++ Templates/profe/ListaAsistencia.php
@@ -1,3 +1,8 @@
+
+<?php
+    // placeholder
+    $nombre_alumno = "Juanito Juanito"; 
+?>
 <!DOCTYPE html>
 <html lang="es">
     <head>
@@ -5,72 +10,76 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Lista de asistencia de alumnos</title>
         <link rel="stylesheet" href="../../Statics/Css/ListadeAsistencia.css">
-        
+        <link rel="stylesheet" href="../../Statics/Css/profeGraph.css">
     </head>
     <body>
-        <main id= "contenido">
-            <div class="tituloListaAsistencia">
-                <h1>61D Asistencia</h1>
-            </div> 
-            <div class="lista-asist">
-                <table class="asistencia-alumno1">
-                    <tbody>
-                        <tr>
-                            <td class="nombalumlista">Juanito Juanito Perez Constantinopla</td>
-                            <td>Fecha</td> 
-                            <td><select class="tipo-asist" name= "tipodeasist">
-                                <option value="asistio">Asistió</option>
-                                <option value="falta">Falta</option>
-                                <option value="justificado">Justificada</option>
-                                </select>
-                            </td>
-                            <td>Fecha</td> 
-                            <td><select class="tipo-asist" name= "tipodeasist">
-                                <option value="asistio">Asistió</option>
-                                <option value="falta">Falta</option>
-                                <option value="justificado">Justificada</option>
-                                </select>
-                            </td>
-                            <td>Fecha</td> 
-                            <td><select class="tipo-asist" name= "tipodeasist">
-                                <option value="asistio">Asistió</option>
-                                <option value="falta">Falta</option>
-                                <option value="justificado">Justificada</option>
-                                </select>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-                <table class="asistencia-alumno2">
-                    <tbody>
-                        <tr>
-                            <td class="nombalumlista">Nombre alumno</td>
-                            <td>Fecha</td> 
-                            <td><select class="tipo-asist" name= "tipodeasist">
-                                <option value="asistio">Asistió</option>
-                                <option value="falta">Falta</option>
-                                <option value="justificado">Justificada</option>
-                                </select>
-                            </td>
-                            <td>Fecha</td> 
-                            <td><select class="tipo-asist" name= "tipodeasist">
-                                <option value="asistio">Asistió</option>
-                                <option value="falta">Falta</option>
-                                <option value="justificado">Justificada</option>
-                                </select>
-                            </td>
-                            <td>Fecha</td> 
-                            <td><select class="tipo-asist" name= "tipodeasist">
-                                <option value="asistio">Asistió</option>
-                                <option value="falta">Falta</option>
-                                <option value="justificado">Justificada</option>
-                                </select>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </main>   
+        <?php include '../../utilities/navbarAlumn.php'; ?>
+        <div class="main-layout">
+            <?php include '../../utilities/sidebarProfe.php'; ?>
+            <main id= "contenido">
+                <div class="tituloListaAsistencia">
+                    <h1>61D Asistencia</h1>
+                </div> 
+                <div class="lista-asist">
+                    <table class="asistencia-alumno1">
+                        <tbody>
+                            <tr>
+                                <td class="nombalumlista">Juanito Juanito Perez Constantinopla</td>
+                                <td>Fecha</td> 
+                                <td><select class="tipo-asist" name= "tipodeasist">
+                                    <option value="asistio">Asistió</option>
+                                    <option value="falta">Falta</option>
+                                    <option value="justificado">Justificada</option>
+                                    </select>
+                                </td>
+                                <td>Fecha</td> 
+                                <td><select class="tipo-asist" name= "tipodeasist">
+                                    <option value="asistio">Asistió</option>
+                                    <option value="falta">Falta</option>
+                                    <option value="justificado">Justificada</option>
+                                    </select>
+                                </td>
+                                <td>Fecha</td> 
+                                <td><select class="tipo-asist" name= "tipodeasist">
+                                    <option value="asistio">Asistió</option>
+                                    <option value="falta">Falta</option>
+                                    <option value="justificado">Justificada</option>
+                                    </select>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <table class="asistencia-alumno2">
+                        <tbody>
+                            <tr>
+                                <td class="nombalumlista">Nombre alumno</td>
+                                <td>Fecha</td> 
+                                <td><select class="tipo-asist" name= "tipodeasist">
+                                    <option value="asistio">Asistió</option>
+                                    <option value="falta">Falta</option>
+                                    <option value="justificado">Justificada</option>
+                                    </select>
+                                </td>
+                                <td>Fecha</td> 
+                                <td><select class="tipo-asist" name= "tipodeasist">
+                                    <option value="asistio">Asistió</option>
+                                    <option value="falta">Falta</option>
+                                    <option value="justificado">Justificada</option>
+                                    </select>
+                                </td>
+                                <td>Fecha</td> 
+                                <td><select class="tipo-asist" name= "tipodeasist">
+                                    <option value="asistio">Asistió</option>
+                                    <option value="falta">Falta</option>
+                                    <option value="justificado">Justificada</option>
+                                    </select>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </main> 
+        </div>  
     </body>
 </html>
     
