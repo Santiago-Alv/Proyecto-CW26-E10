1b90fd1467a491005cd838f9d47d030d1ae31630
Sebastián Méndez Sánchez <sebmndz.04@gmail.com>
2026-06-16 03:46:03 -0600

    Se agrega Recurso alumno actualizado

--- Templates/alumno/RecursoAlumno.php
+++ Templates/alumno/RecursoAlumno.php
@@ -4,7 +4,7 @@
 session_start();
 //include '../../config/config_db.php'; 
 //$conexion = connect();
-//ejemplo(NO CONECTADA A BD todavía)
+/*//ejemplo(NO CONECTADA A BD todavía)
 if (isset($_GET['id_grupo'])) {
     $id_grupo_actual = $_GET['id_grupo'];
 } else {
@@ -26,7 +26,19 @@ $recursos_prueba = [
         'url_imagen' => 'meme.png',
         'url_recurso' => 'https://google.com'
     ]
-];
+];*/
+//PARA BASE DE DATOS ///
+if (isset($_GET['id_grupo'])) {
+    $id_grupo_actual = $_GET['id_grupo'];
+} else {
+    $id_grupo_actual = 61;
+}
+
+$sql = "SELECT * FROM recursos WHERE id_grupo = '" . $id_grupo_actual . "'";
+$resultado_query = mysqli_query($conexion, $sql);
+
+
+
 $nombre_alumn = "Panchito";
 $grupo_alumn = "61D";
 ?>
@@ -53,51 +65,60 @@ $grupo_alumn = "61D";
                 </div>
                 <div class="lista-recursos">
                     <?php
+                    /*Prueba es lo comentado sin BD 
                     if (!empty($recursos_prueba)){
-                        foreach ($recursos_prueba as $recurso) {
+                        foreach ($recursos_prueba as $recurso) {*/
+                    if ($resultado_query && mysqli_num_rows($resultado_query) > 0) {
+                        while ($recurso = mysqli_fetch_assoc($resultado_query)){
                         ?>
                             <div class="tarjeta-recurso-c">
                                 <div class="imagen-recurso-c">
                                 <?php
-                                if ($recurso['url_imagen'] != '' && $recurso['url_imagen'] !== 'temp') {
+                                /*Prueba sin BD
+                                if ($recurso['url_imagen'] != '' && $recurso['url_imagen'] != 'temp') {
                                     $ruta_imagen = "../../Statics/img/recursos/" . $recurso['url_imagen'];
                                     echo "<img src='$ruta_imagen' alt='Imagen del curso' class='img-tarjeta'>";
                                 } else {
                                     echo "<div class='sin-imagen'>Sin Imagen</div>";
-                                }
+                                }*/
+                                if ($recurso['url_imagen'] != "" && $recurso['url_imagen'] != "temp") {
+                                    $ruta_imagen = "../../Statics/img/recursos/" . $recurso['url_imagen'];
+                                    echo "<img src='" . $ruta_imagen . "' class='img-tarjeta'>";
+                                } else {
+                                    echo "<div class='sin-imagen'>Sin Imagen</div>";
+                                } 
                                 ?>
                                 </div>
                                 <div class="contenido-tarjeta-derecha">
                                     <div class="tarjeta-header">
                                         <div class="textos-tarjeta">
-                                            <h3><?php echo strtoupper($recurso['nombre_recurso']); ?></h3>
-                                            <p>(<?php $recurso['descripcion']; ?>)</p>
+                                            <h3><?php echo $recurso['nombre_recurso']; ?></h3>
+                                            <p>(<?php echo $recurso['descripcion']; ?>)</p>
                                         </div>
                                     
                                         <div class="botones-tarjeta">
                                             
                                                 <a href="../../Statics/img/recursos/<?php echo $recurso['url_imagen']; ?>" target="_blank" class="btn-ver">
                                                     Ver imagen
-                                                </a><!-- blank pa q abra otra pestania -->
-                                            <!-- la solucion q se me ocurrio, cerrar el if manual pq no se cerraba normal-->
+                                                </a>
                                         </div>
                                     </div>
                                     <div class="caja-url">
                                         <?php
                                         $url = $recurso['url_recurso'];
-                                        if (substr($url, 0, 4) == "http") {
-                                            echo "<a href= '$url' target='_blanck' style='color: #1d3557; text-decoration: underline; font-weight: bold;'>$url</a>";
+                                        if ($url != "") {
+                                            echo "<a href='$url' target='_blank' style='color: #1d3557; text-decoration: underline; font-weight: bold; '>$url</a>";
                                         } else {
-                                            echo $url;
+                                            echo "No hay un enlace web para este recurso.";
                                         }
                                         ?>
                                     </div>
                                 </div>
                             </div>
-                        <?php //este php es como el tomate de tf2, no se pq si lo quito se rompe todo XDDD
+                        <?php 
                         }
                     } else {
-                        echo "<p style='color: #ffffff;'>Aún no hay recursos asignados a este grupo. ¡Agrega uno para empezar!</p>";
+                        echo "<p style='color: #ffffff;'>Aún no hay recursos asignados a este grupo.</p>";
                     }
                     ?>    
                 </div>
